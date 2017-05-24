59ca1ed73ebb877622e551bfb9a93c101a7d2eb8
Administrator <904620272@qq.com>
2017-05-24 20:15:23 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import {  Routes} from '@angular/router'
 import {LoginComponent} from './login/login-component/login.component';
 
 export const rootRouterConfig:Routes=[
-  {path:'',component:LoginComponent},{}
+  {path:'',component:LoginComponent}
 ];
