67201cae76914bef506f2b9df1aadb252ac6eea8
Administrator <904620272@qq.com>
2017-05-27 12:56:20 +0800

    update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home-component/home.component';
+import { MenuComponent } from './menu-component/menu.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, MenuComponent]
 })
 export class HomeModule { }
