91c9f3cbfac429783a88af9cac08bcc0f7fb65cc
FunClub <904620272@qq.com>
2017-06-01 20:46:39 +0800

    update

---
+++ src/app/update-seller/update-seller-model/SellerAndShopModel.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/6/1.
+ */
+export class SellerAndShopModel{
+  sellerId:number;
+  sellerPassword:string;
+  sellerName:string;
+  sellerTelephone:string;
+  sellerAddress:string;
+  shopId:number;
+  shopName:string;
+  shopImg:string;
+  leastPrice:string;
+  shopType:any;
+  shopNotice:string;
+}
+
